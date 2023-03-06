9bc64852c3bb3221f15e99b4d2da67ed918bb0b5
Josh <jsohua.butts4477@gmail.com>
2023-03-06 00:54:34 -0600

    3rd itteration of probably failing code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,28 @@
-var a =$('#option1')
-var b =$('#option2')
-var c =$('#option3')
-var d =$('#option4')
 
-const questionEl = $("question")
-const answerButtonsEl = $("answer-buttons")
 
-const currentQuestionIndex=('')
-function setNextQuestion(){
-showQuestion(currentQuestionIndex)
-}
-function showQuestion(question){
-
-}
-function selectAnswer(){
-
+var question = {
+  title: '9+10',
+  al: ["19","4","3","21"],
+  correctAnswer: 1
 }
 
-const questions =[
-{
-question: "whats 9+10?",
-answers: [
-  {text: "4", correct:true},
-  {text: "22", correct:false}
-]
+function showQuestion(q){
+  //jquery did not want to work for this
+  var titleEl = document.getElementById("title")
+
+  titleEl.textContent = q.title;
+
+ var alts = $('.al')//document.querySelectorAll('.al')
+ console.log(alts)
+ alts.forEach(function(element, index){
+  element.textContent = q.al[index];
+ 
+});
+
+  // alts.array.forEach(element => {
+  //   element.textContent = "a"
+  // });
+
 }
 
-]
+showQuestion(question);
