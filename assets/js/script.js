450324486ac89b2ee1b580f93e18b8024cb71611
Josh <jsohua.butts4477@gmail.com>
2023-03-10 03:36:00 -0600

    finished quiz logic, and timer functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,52 @@
+startTimer()
 var CorrectCount = 0
 var IncorrectCount = 0
 var i
-startTimer()
+var paused = false
 var alts = $('.al')//document.querySelectorAll('.al')
 var timer;
 var timerCount;
+var timerEnd = setInterval(function() {
+  if(paused === true) {
+    timerCount++;
+  }
+}, 1000);
+
 var timerElement = document.querySelector(".timer");
 var qIndex = 0
 var question = [{
-  title: '9+10',
-  al: ["19","4","3","21"],
-  correctAnswer: 0
-},
-{
-  title: 'is water wet',
-  al: ["yes","no","maybe","kinda"],
-  correctAnswer: 1
-},
-{
-  title: 'Question1',
-  al: ["1","2","3","4"],
-  correctAnswer: 1
-},
-{
-  title: 'Question2',
-  al: ["1","2","3","4"],
+  title: '1. Commonly used data types DO NOT include:',
+  al: ["strings","booleans","alerts","numbers"],
   correctAnswer: 2
 },
 {
-  title: 'Question3',
-  al: ["1","2","3","4"],
+  title: '2. The condition in an if / else statement is enclosed with _________',
+  al: ["quotes","curly brackets","parenthesis","square brackets"],
+  correctAnswer: 2
+},
+{
+  title: '3. Arrays in JavaScript can be used to store ________',
+  al: ["numbers and strings","other arrays","booleans","all of the above"],
   correctAnswer: 3
 },
 {
-  title: 'Question4',
-  al: ["1","2","3","4"],
-  correctAnswer: 0
+  title: '4. String values must be enclosed within _____ when being assigned to variables',
+  al: ["commas","curly brackets","quotes","parenthesis"],
+  correctAnswer: 2
+},
+{
+  title: '5. A very useful tool used during development and debugging for printing content to the debugger is:',
+  al: ["JavaScript","terminal/bash","for loops","console.log"],
+  correctAnswer: 3
+},
+{
+  title: "6. What will the code below output to the console? console.log(1 +  +'2' + '2');",
+  al: ["32","122","13","14"],
+  correctAnswer: 0,
 }
+
+//function here
+// questionEnd
 ]
 function showQuestion(q){
   //jquery did not want to work for this
@@ -56,13 +66,14 @@ function showQuestion(q){
 
 });
 }
-  // current bug:
-$(this).click(function(e){
+$('.al').click(function(e){
+  console.log(qIndex)
   console.log(e.target)
-  
   i=e.target.id
-  // this e.target.id will generate a click for every single element on the page
-  // as seen by it console logging the elemnt/body of what you click
+  if (qIndex == 5){
+    console.log('end')
+    questionEnd()
+  } 
   if (question[qIndex].correctAnswer == i){
     correct()
     qIndex++
@@ -73,12 +84,11 @@ $(this).click(function(e){
     qIndex++
    showQuestion(question)
   }
+  
 });
-showQuestion(question);
-
 function startTimer() {
   // Sets timer
-  timerCount = 21
+  timerCount = 121
   timer = setInterval(function() {
     timerCount--;
     timerElement.textContent = ('Time: ' + timerCount);
@@ -86,10 +96,9 @@ function startTimer() {
       $('.list-group').empty();
       $('#title').empty();
       $('.result').empty();
-      $( ".result" ).append( "<h1>You ran out of time!</h1> <br><h2>Your score was: " +  (CorrectCount) +"</h2> <h3>Refresh the page to try again or checkout your high score at the top right!</h3>" );
+      $( ".result" ).append( "<h1>You ran out of time!</h1> <br><h2>Your score was: " +  (CorrectCount) +"</h2> <h3>Refresh the page to try again or check out your high score at the top left!</h3>" );
       clearInterval(timer);
     }
-    
   }, 1000);
 }
 function correct(){
@@ -97,15 +106,23 @@ function correct(){
       CorrectCount += 1
       $('.result').empty();
       $( ".result" ).append( "<h1>Correct!</h1>" );
-      // console.log(CorrectCount)
-      // console.log(IncorrectCount)
 }
 function incorrect(){
   console.log("incorrect")
       IncorrectCount += 1
-      timerCount -= 5
+      timerCount -= 10
       $('.result').empty();
       $( ".result" ).append( "<h1>Incorrect</h1>" );
-      // console.log(CorrectCount)
-      // console.log(IncorrectCount)
 }
+function questionEnd(){
+$('.list-group').empty();
+$('#title').empty();
+$( "#title" ).append( "<h1>You completed the quiz!</h1> <br><h2>Your score was: " +  (CorrectCount) +"</h2> <h3>Refresh the page to try again or check out your high score at the top left!</h3>" );
+paused = true
+}
+
+// console.log(lastQuestion)
+// if (CorrectCount + IncorrectCount ){
+//   questionEnd()
+// }
+showQuestion(question);
